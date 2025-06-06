50ba201b2918fccdc539cdcc8230f0db64e74652
Alexander Rieß <alexander.riess@icinga.com>
2025-06-06 11:52:17 +0200

    Improve report output

--- reports.py
+++ reports.py
@@ -78,23 +78,29 @@ def createMarkdownReport(diff: LabelDiff):
     if len(diff.missing) > 0:
         out += "\n### Missing Labels (Create)\n\n"
         for label in diff.missing:
-            out += f" - {label['name']}: {label['description']}\n"
+            out += f" - {label['name']}"
+            if "description" in label and label["description"] != "":
+                out += f": {label['description']}"
+            out += "\n"
 
     if len(diff.extra) > 0:
         out += "\n### Extra Labels (Delete)\n\n"
         for label in diff.extra:
-            out += f" - {label['name']}: {label['description']}\n"
+            out += f" - {label['name']}"
+            if "description" in label and label["description"] != "":
+                out += f": {label['description']}"
+            out += "\n"
 
     if len(diff.diff) > 0:
         out += "\n### Different Labels (Modify)\n\n"
         for label in diff.diff:
             out += f" - {label['truth']['name']}\n"
             if "color" in label["delta"]:
-                out += f"   - Change color from: '{label['actual']['color']}' to '{label['truth']['color']}'\n"
+                out += f"   - Change color from '{label['actual']['color']}' to '{label['truth']['color']}'\n"
             if "description" in label["delta"]:
-                out += f"   - Change description from: '{label['actual']['description']}' to '{label['truth']['description']}'\n"
+                out += f"   - Change description from '{label['actual']['description']}' to '{label['truth']['description']}'\n"
             if "name" in label["delta"]:
-                out += f"   - Rename from: '{label['actual']['name']}' to '{label['truth']['name']}'\n"
+                out += f"   - Rename from '{label['actual']['name']}' to '{label['truth']['name']}'\n"
 
     return out
 
@@ -107,16 +113,17 @@ def terminalPrint(diff: LabelDiff, action: str, label: dict):
         print(f"create '{label['name']}' ({label['description']})")
     elif action == "modify":
         changes = []
+        name = label["actual"]["name"]
         if "color" in label["delta"]:
             changes.append(
-                f"change color of '{label['actual']['name']}' from '{label['actual']['color']}' to '{label['truth']['color']}'",
+                f"change color of '{name}' from '{label['actual']['color']}' to '{label['truth']['color']}'",
             )
         if "description" in label["delta"]:
             changes.append(
-                f"change description of '{label['actual']['name']}' from '{label['actual']['description']}' to '{label['truth']['description']}'",
+                f"change description of '{name}' from '{label['actual']['description']}' to '{label['truth']['description']}'",
             )
         if "name" in label["delta"]:
             changes.append(
-                f"rename from '{label['actual']['name']}' to '{label['truth']['name']}'",
+                f"rename from '{name}' to '{label['truth']['name']}'",
             )
         print(", ".join(changes))
